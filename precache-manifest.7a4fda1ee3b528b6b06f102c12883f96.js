6a8119c5dcfb190be9a0b306a0f86d2ede1231c5
piyush@crive.me <piyush@crive.me>
2019-07-03 14:33:10 +0530

    Updates

---
+++ precache-manifest.7a4fda1ee3b528b6b06f102c12883f96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a764dd22beede7fd53698363ed09ee19",
+    "url": "/snake_game/index.html"
+  },
+  {
+    "revision": "1c2a9a0e3102702d829e",
+    "url": "/snake_game/static/css/main.ed3c93e2.chunk.css"
+  },
+  {
+    "revision": "130beed1555053dbc7e4",
+    "url": "/snake_game/static/js/2.92be6dbf.chunk.js"
+  },
+  {
+    "revision": "1c2a9a0e3102702d829e",
+    "url": "/snake_game/static/js/main.07192094.chunk.js"
+  },
+  {
+    "revision": "d5ede808ef67c106ab85",
+    "url": "/snake_game/static/js/runtime~main.5f752313.js"
+  }
+]);
