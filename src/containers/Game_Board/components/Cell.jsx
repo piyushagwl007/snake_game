f6fe00538c130144cd6a988186837eb358a3c149
piyush@crive.me <piyush@crive.me>
2019-07-07 20:02:41 +0530

    REMOVED PADDING FOR BETTER DISPLAY

--- src/containers/Game_Board/components/Cell.jsx
+++ src/containers/Game_Board/components/Cell.jsx
@@ -1,5 +1,4 @@
 import React, { PureComponent } from "react";
-import CellStates from "../../../constants/CellStates";
 //react reduct connector
 import { connect } from 'react-redux'
 
@@ -11,35 +10,14 @@ import {initializeCell} from "../../../actions/Init"
 class Cell extends PureComponent {
   constructor(props) {
     super();
-    let snakeFound = props.snake.findIndex(snakeHead => {
-        // if(snakeHead.row == props.row && snakeHead.column === props.column)
-        // // console.log("HERE==>>",snakeHead.row , props.row,  snakeHead.column, props.column)
-        return snakeHead.row === props.row && snakeHead.column === props.column;
-      }) > -1;
 
     this.state = {
       row: props.row,
-      column: props.column,
-      value: snakeFound ? CellStates.HAVE_SNAKE : props.foodCell?CellStates.HAVE_FOOD:CellStates.HAVE_NOTHING
+      column: props.column
     };
   }
 
-  componentDidUpdate(prevProps, prevState) {
-      // console.log("The prev props",prevProps)
-    let snakeFound =
-      this.props.snake.findIndex(snakeHead => {
-        return (
-          snakeHead.row === this.state.row &&
-          snakeHead.column === this.state.column
-        );
-      }) > -1;
-    if (snakeFound) this.setState({ value: CellStates.HAVE_SNAKE });
-    else if (this.props.foodCell)
-      this.setState({ value: CellStates.HAVE_FOOD });
-    else 
-      this.setState({ value: CellStates.HAVE_NOTHING });
-  }
-
+  
   //Initializing the Cell Component
   componentDidMount(){
     // console.log("COMPONENT MOUNTED")
